5d7b1d3ee8f345b25623866be25ba4bc8957f09c
MadLab <hello@madlab.design>
2022-08-20 00:50:00 +0200

    Styled-components and nvm version control

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import '@aws-amplify/ui-react/styles.css';
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import styled from 'styled-components';
 Amplify.configure(awsconfig);
 function App() {
   return (
     <Authenticator>
     {({ signOut, user }) => (
-      <main>
+      <Main>
         <h1>Hello {user?.username}</h1>
         <button onClick={signOut}>Sign out</button>
-      </main>
+      </Main>
     )}
   </Authenticator>
   );
 }
 
 export default withAuthenticator(App);
+
+const Main = styled.main`
+background-color: red ;
+`
