f5d9cef7a0a23fa4b1aac3fd5592615e6c158c01
MadLab <hello@madlab.design>
2022-08-19 22:31:15 +0200

    AWS Amplify with and ui-react

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "mldauthpaged2a4d754": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
